01f118e15468d196a728c9fe88085401b9858e30
Pedro Moura <pedro030707@gmail.com>
2026-05-10 16:27:00 -0300

    style: Estilizando Home e Sobre + JS para o Hero

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+    const intro = document.getElementById("inicio");
+
+    setTimeout(()=>{
+        intro.classList.add("efeito-out");
+
+        setTimeout(()=>{
+         intro.style.display="none";
+         content.classList.remove("hidden");
+         content.classList.add("efeito-in");
+        }, 1000)
+    }, 3000)
+}
